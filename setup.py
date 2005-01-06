da35dfb3e78685f33f5bc22a14812986c1e7100d
Matthew Nicholson <matt@matt-land.com>
2005-01-06 01:13:25 +0000

    Bumped version number in setup.py
    ...and removed PKG-INFO file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name = 'pyst',
-      version = '0.0.4rc6',
+      version = '0.0.4rc7',
       description = 'Asterisk related utility modules.',
       author = 'Karl Putland',
       author_email = 'kputland@users.sourceforge.net',
