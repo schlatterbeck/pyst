293fcce76d05c3e756d5152197ec753e4148e7d9
Matthew Nicholson <matt@matt-land.com>
2006-09-20 00:52:18 +0000

    setup.py: Updated version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name = 'pyst',
-      version = '0.1.0',
+      version = '0.2',
       description = 'Asterisk related utility modules.',
       author = 'Karl Putland',
       author_email = 'kputland@users.sourceforge.net',
